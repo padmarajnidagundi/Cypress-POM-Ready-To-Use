9097fc8d4fad45d86434a25a683a9fd609464d20
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2025-03-01 21:40:53 +0200

    origin/main

--- cypress/e2e/API-Tests.cy.js
+++ cypress/e2e/API-Tests.cy.js
@@ -1,26 +1,57 @@
 describe('API Testing', () => {
 
-    Cypress.config('baseUrl', 'http://dummy.restapiexample.com/api/v1')
+    describe('API Testing', () => {
 
-    it('Test 1 - GET - read', () => {
-        cy.request('/employees').then((response) => {
-            expect(response).to.have.property('status', 200)
-            expect(response.body).to.not.be.null
-            expect(response.body.data).to.have.length(24)
+        Cypress.config('baseUrl', 'https://reqres.in')
+    
+        it('Test 1 - GET API testing Using API ', () => {
+            cy.request('/api/users?page=2').then((response) => {
+                expect(response).to.have.property('status', 200)
+                expect(response.body).to.not.be.null
+                expect(response.body).to.have.property('page', 2)
+                expect(response.body.data).to.be.an('array')
+                expect(response.body.data).to.have.length(6)
+                response.body.data.forEach(user => {
+                    expect(user).to.have.all.keys('id', 'email', 'first_name', 'last_name', 'avatar')
+                })
+            })
         })
+    
+        it('Test 2 - POST API testing Using API', () => {
+            cy.request("POST", "/api/users", {
+                name: "morpheus",
+                job: "leader",
+              }).should((response) => {
+                expect(response.status).to.eq(201);
+                expect(response.body).to.have.property('name', 'morpheus')
+                expect(response.body).to.have.property('job', 'leader')
+                expect(response.body).to.have.property('id')
+                expect(response.body).to.have.property('createdAt')
+              });
+           });
+     
+    
+        it('Test 3 - PUT API testing Using Flip', () => {
+            cy.request("PUT", "/api/users/2", {
+                name: "QAAutomationLabs",
+                job: "QA Automation Engg",
+              }).should((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.body).to.have.property('name', 'QAAutomationLabs')
+                expect(response.body).to.have.property('job', 'QA Automation Engg')
+                expect(response.body).to.have.property('updatedAt')
+              });
+            });
+    
+    
+        it("Test 4 - DELETE API testing Using Cypress API", () => {
+            cy.request("DELETE", "/api/users/2").should((response) => {
+                expect(response.status).to.eq(204);
+                expect(response.body).to.be.empty
+                });
+              });
+    
     })
+    
 
-    it('Test 2 - POST - create', () => {
-        const item = { "name": "test", "salary": "123", "age": "23" }
-        cy.request('POST', '/create', item)
-            .its('body')
-            .its('data')
-            // .should('deep.eq', item)
-            .should('include', { name: 'test' })
-    })
-
-    it('Test 3 - PUT - update', () => {
-        const item = { "name": "test1" }
-        cy.request({ method: 'PUT', url: '/update/1', body: item, failOnStatusCode: false }).its('status').should('eq', 401)
-    })
 })
