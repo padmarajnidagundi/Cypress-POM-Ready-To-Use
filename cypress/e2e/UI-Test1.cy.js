a13202aef6a94ba7d1221f2e5b8643f9f564422e
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2025-03-02 15:22:10 +0200

    origin/main

---
+++ cypress/e2e/UI-Test1.cy.js
@@ -0,0 +1,20 @@
+import ExamplePage from '../pageObjects/homePageLinks'
+
+
+describe('Example Cypress.io Tests', () => {
+    const examplePage = new ExamplePage()
+
+    beforeEach(() => {
+        examplePage.visit()
+    })
+
+    it('should verify the header text', () => {
+        examplePage.getHeader().should('contain', 'Kitchen Sink')
+    })
+
+    it('should navigate to the "Utilities" page', () => {
+        examplePage.getNavigationLink('Utilities').click()
+        cy.url().should('include', '/utilities')
+        cy.get('h1').should('contain', 'Utilities')
+    })
+})
