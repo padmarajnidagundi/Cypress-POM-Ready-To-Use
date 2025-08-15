93dc083471d1b4f04867db7f0903b4ce60da7d90
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2025-08-15 17:20:20 +0300

    origin/main

--- cypress/e2e/additionalTests.cy.ts
+++ cypress/e2e/additionalTests.cy.ts
@@ -1,4 +1,5 @@
 describe('Additional Tests', () => {
+  // Test visiting the homepage and verifying key elements
   it('should visit the homepage', () => {
     cy.visit('https://wesendcv.com/');
     cy.url().should('eq', 'https://wesendcv.com/');
@@ -7,6 +8,7 @@ describe('Additional Tests', () => {
     cy.get('header').should('exist');
   });
 
+  // Test for the presence and state of the Login button
   it('should check for a login button', () => {
     cy.visit('https://wesendcv.com/');
     cy.get('button').contains('Login').should('be.visible').and('not.be.disabled');
