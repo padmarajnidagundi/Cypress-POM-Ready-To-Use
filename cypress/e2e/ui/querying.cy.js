25d1b331547ade90f2ef196974a2788570a0de47
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2025-11-09 14:37:58 +0200

    origin/main

--- cypress/e2e/ui/querying.cy.js
+++ cypress/e2e/ui/querying.cy.js
@@ -1,2 +1,35 @@
 import HomePageLinks from '../../pageObjects/pages/queryingPageLinks'
 // ...existing code from UI-Test2.cy.js...
+
+describe('Advanced DOM Querying Tests', () => {
+  const homePage = new HomePageLinks()
+
+  beforeEach(() => {
+    homePage.visit()
+  })
+
+  it('should traverse DOM hierarchy correctly', () => {
+    cy.get('.query-list')
+      .children()
+      .should('have.length.gt', 0)
+      .first()
+      .parent()
+      .should('have.class', 'query-list')
+  })
+
+  it('should verify element states', () => {
+    cy.get('button.query-btn')
+      .should('be.visible')
+      .and('be.enabled')
+      .invoke('attr', 'type')
+      .should('eq', 'button')
+  })
+
+  it('should handle dynamic content loading', () => {
+    cy.get('.query-button').first().click()
+    cy.get('.dynamic-content')
+      .should('exist')
+      .and('be.visible')
+      .and('not.be.empty')
+  })
+})
